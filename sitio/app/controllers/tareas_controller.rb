c7ea11e52dfb9725d538fd033b778a08c707e503
Angélica MT <angy.mayora@gmail.com>
2018-10-04 16:32:29 -0500

    v.3 rails

--- sitio/app/controllers/tareas_controller.rb
+++ sitio/app/controllers/tareas_controller.rb
@@ -2,18 +2,34 @@ class TareasController < ApplicationController
   def index
     @tareas=Tarea.all
   end
+  
   def new
     @tarea=Tarea.new
   end
+  
   def create
     @tarea = Tarea.new(titulo: params[:tarea][:titulo],descripcion: params[:tarea][:descripcion])
     if @tarea.save
-     
+     redirect_to controller: 'tareas',action: 'show' , id: @tarea.id
     else
+
       render :new
     end
   end  
+  
   def show
     @tarea= Tarea.find(params[:id])
   end
+
+  def edit
+    @tarea= Tarea.find(params[:id])
+    
+  end
+
+  def destroy
+    @tarea= Tarea.find(params[:id]) 
+    @tarea.destroy
+    redirect_to controller: 'tareas',action: 'index'
+  end  
+
 end
--- sitio/config/routes.rb
+++ sitio/config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   post 'tareas',to: 'tareas#create'
   get 'welcome/index'
   root 'welcome#index'
-  get 'tareas/:id',to: 'tareas#show'  
+  get 'tareas/:id',to: 'tareas#show'
+  delete 'tareas/:id', to: 'tareas#destroy'
+  get 'tareas/:id/edit', to: 'tareas#edit'   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
